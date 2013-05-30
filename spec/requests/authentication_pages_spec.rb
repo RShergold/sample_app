52a65908a0d1a347d5da8e0ef0c52b17bd0f2488
remi shergold <remi.shergold@hotmail.com>
2013-05-30 17:09:35 +0100

    Add user microposts

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -111,6 +111,22 @@ describe "Authentication" do
         
       end
       
+      describe "in the Microposts controler" do
+        
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { response.should redirect_to(signin_path) }
+        end
+        
+        describe "submitting to the destroy action" do
+          before do
+            micropost = FactoryGirl.create(:micropost)
+            delete micropost_path(micropost)
+          end
+          specify { response.should redirect_to(signin_path) }
+        end
+        
+      end
       
     end
     
